495a480c6d53c5ef9975cd7fdec348c5adbdb265
nowhereland243 <nolan.z.feng@gmail.com>
2025-10-17 01:34:56 -0400

    Add UTM parameter tracking to Umami Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,35 @@ export default function RootLayout({
           defer
           src="https://snapboo-analytics.vercel.app/script.js"
           data-website-id="bdd16d84-aeeb-40fc-b586-3082ca06ecc9"
+          data-auto-track="true"
+        />
+        {/* UTM Parameter Tracking */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                if (typeof window !== 'undefined') {
+                  window.addEventListener('load', function() {
+                    var params = new URLSearchParams(window.location.search);
+                    var utmData = {};
+                    
+                    // Capture UTM parameters
+                    ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'].forEach(function(param) {
+                      var value = params.get(param);
+                      if (value) {
+                        utmData[param] = value;
+                      }
+                    });
+                    
+                    // Send to Umami if any UTM parameters exist
+                    if (Object.keys(utmData).length > 0 && window.umami) {
+                      window.umami.track('utm-visit', utmData);
+                    }
+                  });
+                }
+              })();
+            `,
+          }}
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
