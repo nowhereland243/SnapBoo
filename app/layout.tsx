40c436e02fffd3e3ead43b0645dc8260e7c5ef10
nowhereland243 <nolan.z.feng@gmail.com>
2025-10-17 01:52:40 -0400

    Add malicious bot filtering to analytics (keeps good bots like Googlebot)

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,45 @@ export default function RootLayout({
           data-website-id="bdd16d84-aeeb-40fc-b586-3082ca06ecc9"
           data-auto-track="true"
         />
-        {/* UTM Parameter Tracking */}
+        {/* UTM Parameter Tracking with Malicious Bot Filtering */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
               (function() {
                 if (typeof window !== 'undefined') {
+                  // Detect malicious bots (but keep good bots like Googlebot, Facebook, etc.)
+                  function isMaliciousBot() {
+                    var ua = navigator.userAgent.toLowerCase();
+                    var maliciousPatterns = [
+                      'curl/',
+                      'wget/',
+                      'python-requests',
+                      'scrapy',
+                      'axios/',
+                      'node-fetch',
+                      'go-http-client',
+                      'apache-httpclient',
+                      'nikto',
+                      'sqlmap',
+                      'masscan',
+                      'nmap',
+                      'headless',
+                      'phantom',
+                      'selenium',
+                      'webdriver'
+                    ];
+                    
+                    return maliciousPatterns.some(function(pattern) {
+                      return ua.includes(pattern);
+                    });
+                  }
+                  
                   window.addEventListener('load', function() {
+                    // Skip tracking if malicious bot detected
+                    if (isMaliciousBot()) {
+                      return;
+                    }
+                    
                     var params = new URLSearchParams(window.location.search);
                     var utmData = {};
                     
